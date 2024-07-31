39e150e80607f2ff76c5df1d3831f90964c70eff
Madhav7865 <bahetimadhav70@gmail.com>
2024-07-31 23:05:41 +0530

    note-added

--- src/Pic.js
+++ src/Pic.js
@@ -1,16 +1,16 @@
-import React, {useEffect } from 'react'
+import React, { useEffect } from 'react';
 import './pic.css';
 
 function Pic() {
   useEffect(() => {
     const letters = document.querySelectorAll('.letter');
     letters.forEach((letter, index) => {
-        letter.style.animationDelay = `${index * 0.1}s`; // Delay each letter's animation
+      letter.style.animationDelay = `${index * 0.08}s`;
     });
-}, []);
-    
-      const note = "Heyyloo Smradhi. A very special day ";
-    
+  }, []);
+
+  const note = "Heyyloo Smradhi. A very special day. You have been truly amazing and I cherish this bond between us. You have been there constantly with me. Just enjoy every moment of your life because zindagi na milegi doobara. I will always be grateful for this IK MULAQAAT. Enjoy your day. Happy Birthday Favourite ";
+
   return (
     <div className="pic-container">
       <img className="us" src="us.jpg" alt="Us" />
@@ -18,9 +18,10 @@ function Pic() {
         {note.split('').map((char, index) => (
           <span key={index} className={`letter ${char === ' ' ? 'space' : ''}`}>{char}</span>
         ))}
+        <span className="letter" style={{ animationDelay: `${note.length * 0.08}s` }}> 😊 🌸 😊</span>
       </p>
     </div>
-  )
+  );
 }
 
-export default Pic
+export default Pic;
